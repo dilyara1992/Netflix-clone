292f1d96fbb6d6b5c6d1cff367b1c4b47edfb54d
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-04-02 14:03:23 -0700

    resolved problems with signin and signup page

--- src/index.js
+++ src/index.js
@@ -2,25 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './App';
+import { firebase } from './lib/firebase.prod';
 import { FirebaseContext } from './context/firebase';
-import { initializeApp } from 'firebase/app';
 
 
-const config = {
-  apiKey: "AIzaSyAlWg6UhUfjzci79m_kqUaxEgwT1RWI8jA",
-  authDomain: "netflix-clone-ec090.firebaseapp.com",
-  projectId: "netflix-clone-ec090",
-  storageBucket: "netflix-clone-ec090.appspot.com",
-  messagingSenderId: "214865636283",
-  appId: "1:214865636283:web:9b6ce17aa0ed5cf458b29e"
-};
-
-// const firebase = window.firebase.initializeApp(config);
-
-const firebase  = initializeApp(config)
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={{firebase: firebase}}>
+  <FirebaseContext.Provider value={{firebase}}>
         <GlobalStyles />
         <App />
     </FirebaseContext.Provider>, 
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,28 @@
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { seedDatabase } from '../seed';
+
+//process.env.REACT_API_KEY
+
+const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const AUTH_DOM = process.env.REACT_APP_FIREBASE_DOMAIN;
+const PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const STORE_BUCKET = process.env.REACT_APP_FIREBASE_STOAGE_BUCKET;
+const MESS_SENDID = process.env.REACT_APP_FIREBASE_MESSAGE_ID;
+const APP_ID = process.env.REACT_APP_FIREBASE_ID;
+
+const config = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOM,
+  projectId: PROJECT_ID,
+  storageBucket: STORE_BUCKET,
+  messagingSenderId: MESS_SENDID,
+  appId: APP_ID
+};
+
+const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
+
+export { firebase };
