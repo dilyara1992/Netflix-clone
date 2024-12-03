eb745af7153365c2fc393bfe5b89770dcb01b877
Tulegenova, Dilyara <dtulegenova3@gatech.edu>
2024-12-03 00:36:38 -0800

    Updates

---
+++ precache-manifest.acc9b075f17ae0eb5a6fc6842108e831.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2de1af6ded06cf911943",
+    "url": "/dilyara1992/Netflix-clone/static/js/main.2de1af6d.chunk.js"
+  },
+  {
+    "revision": "fd048ea7dc84ff0a8569",
+    "url": "/dilyara1992/Netflix-clone/static/js/1.fd048ea7.chunk.js"
+  },
+  {
+    "revision": "0cd1ddc5ba25170cf836",
+    "url": "/dilyara1992/Netflix-clone/static/js/runtime~main.0cd1ddc5.js"
+  },
+  {
+    "revision": "a900767ed2c0b6849707e68701ec820b",
+    "url": "/dilyara1992/Netflix-clone/index.html"
+  }
+];
